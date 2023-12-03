7b1082633974e790bc36b4fec82ddaff65c1e47f
bruno <bruno.s.carmena@hotmail.com>
2023-12-03 20:42:13 -0300

    primer commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+let parcial = "";
+let operRealizada = document.getElementById("operacionRealizada");
+let operadorSeleccionado = "";
+let numero = "";
+let ultimoDigito = "";
+
+
+function operador(num){
+    numero = numero + num;
+
+    parcial = parcial + num;
+
+    operRealizada.innerHTML = parcial;
+    if(numero == '0' && operadorSeleccionado == '/'){
+        limpiar();
+        txtResul.innerHTML = "Indefinido";
+        return;
+    }
+    if(ultimoDigito = 'operacion'){
+        ultimoDigito = 'numero'
+    }
+}
+
+function operacion(oper){
+    operadorSeleccionado = oper;
+    ultimoDigito = 'operacion';
+    parcial = parcial + oper;
+    numero = 0;
+    operRealizada.innerHTML = parcial;
+}
+
+function calculo(){
+    parcial = eval(parcial);
+    parcial = parcial.toString();
+    numero = "";
+    operRealizada.innerHTML = parcial;
+}
+
+
+
+function limpiar(){
+    operadorSeleccionado = "";
+    parcial = "";
+    numero = "";
+    operRealizada.innerHTML = 0;
+}
